0d6c7018fc157072f418772277d05cfa74dbd7cd
vidit0879 <viditpandey621nit@gmail.com>
2025-04-18 01:10:28 +0530

    add:about

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './index.css'; 
+
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 const client=new ApolloClient({
   uri: 'http://localhost:4000/api/graphql',
