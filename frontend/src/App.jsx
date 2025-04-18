c7a9423ee5e513f620c71c3600ea91d6a465e127
Payal Bande <payalbande3@gmail.com>
2025-04-18 16:38:05 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {  Route, Router, Routes } from 'react-router-dom'
 import About from './components/About/AboutOrganizers'
 import Home from './components/Home/Home'
 import ImportantDates from './components/ImportantDates/ImportantDates'
+import Speaker from './components/Speaker/Speaker'
 function App() {
   const [count, setCount] = useState(0)
 //   const {loading,error, data} = useQuery(gql`
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About />} />
           <Route path="/dates" element={<ImportantDates/>}/>
+          <Route path="/speaker" element={<Speaker/>}/>
         </Routes>
     </main>
     
--- frontend/src/components/ImportantDates/ImportantDates.jsx
+++ frontend/src/components/ImportantDates/ImportantDates.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import { CalendarClock } from "lucide-react";
+// import { CalendarClock } from "lucide-react";
 
 const Dates = [
   {
